1e799fe4f7b7bdb6961f3bc010d64c14833cd7e3
Bakhyt1987 <bzh.root87@gmail.com>
2023-12-13 05:46:50 +0600

    use cypress commands and chaining

--- cypress/e2e/automation-test-store/variables-and-cy-commands.js
+++ cypress/e2e/automation-test-store/variables-and-cy-commands.js
@@ -3,10 +3,6 @@
 describe("Inspect Automation Test Store items using chain of commands", () => {
     it("Navigating to specific product pages", () => {
         cy.visit("https://automationteststore.com/");
-        // const makeupLink = cy.get("a[href*='product/category&path=']").contains("Makeup")
-        // const skincareLink = cy.get("a[href*='product/category&path=']").contains("Skincare")
-        // makeupLink.click();
-        // skincareLink.click();
         cy.get("a[href*='product/category&path=']").contains("Makeup").click();
         cy.get("a[href*='product/category&path=']").contains("Skincare").click();
     });
@@ -18,7 +14,7 @@ describe("Inspect Automation Test Store items using chain of commands", () => {
         cy.log(header);
     });
 
-    it.only("Navigating to specific product pages", () => {
+    it("Navigating to specific product pages", () => {
         cy.visit("https://automationteststore.com/");
         cy.get("a[href*='product/category&path=']").contains("mAKEUP",{matchCase:false}).click()
         cy.get('h1 .maintext').then(($headerText) => {
@@ -26,7 +22,19 @@ describe("Inspect Automation Test Store items using chain of commands", () => {
             cy.log("Found header Text: " + headerText)
             expect(headerText).is.eq('Makeup');
         })
+    });
+
+    it.only("Validate properties of the Contact Us Page", () => {
+        cy.visit("https://automationteststore.com/index.php?rt=content/contact");
+        // Uses cypress commands and chaining
+        cy.contains("#ContactUsFrm","Contact Us Form").find("#field_11").should("contain","First name"); 
+
+        //jquery approach
+
+        //embedded commands
 
 
     });
+
+
 })   
