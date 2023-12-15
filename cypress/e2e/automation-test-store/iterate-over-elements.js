ed1c11e616ad0188822819f4d12423696aa43e69
Bakhyt1987 <bzh.root87@gmail.com>
2023-12-15 15:42:59 +0600

    iterate over elements

--- cypress/e2e/automation-test-store/iterate-over-elements.js
+++ cypress/e2e/automation-test-store/iterate-over-elements.js
@@ -1,19 +1,27 @@
-/// <reference types="cypress" /> 
+/// <reference types="cypress" />
 
 describe("Iterate over elements", () => {
-    it("Log information of all hair care products", () => {
-        cy.visit("https://automationteststore.com/");
-        cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
-        
-    });
+  it("Log information of all hair care products", () => {
+    cy.visit("https://automationteststore.com/");
+    cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
 
-    it("Add specific product to basket", () => {
-        cy.visit("https://automationteststore.com/");
-        cy.get("a[href*='product/category&path=']").contains("Hair care",{matchCase:false}).click();
-        const header = cy.get("h1 .maintext");
-        cy.log(header);
+    cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
+      cy.log("Index: " + index + " : " + $el.text());
     });
+  });
+
+  it("Add specific product to basket", () => {
+    cy.visit("https://automationteststore.com/");
+    cy.get("a[href*='product/category&path=']")
+      .contains("Hair care", { matchCase: false })
+      .click();
+    const header = cy.get("h1 .maintext");
+    cy.log(header);
+
+    cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
+      if ($el.text().startsWith("Curls to straight Shampoo")) {
+        cy.wrap($el).click();
+      }
+    });
+  });
 });
-
-
- 
