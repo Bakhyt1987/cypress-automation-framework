c8ca288ab254d1ffc97e237c69568c5ca4ac0a51
Bakhyt1987 <bzh.root87@gmail.com>
2023-12-15 09:34:47 +0600

    debug iterate

--- cypress/e2e/automation-test-store/iterate-over-elements.js
+++ cypress/e2e/automation-test-store/iterate-over-elements.js
@@ -3,7 +3,7 @@
 describe("Iterate over elements", () => {
     it("Log information of all hair care products", () => {
         cy.visit("https://automationteststore.com/");
-        cy.get("a[href*='product/category&path=']").contains("Hair care").click();
+        cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
         
     });
 
