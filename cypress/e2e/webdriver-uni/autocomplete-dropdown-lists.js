e4ba151c4ac08986e4c3bf072d8680e3d3497ad3
Bakhyt1987 <bzh.root87@gmail.com>
2024-01-02 20:47:35 +0600

    autocomplete dropdown list

---
+++ cypress/e2e/webdriver-uni/autocomplete-dropdown-lists.js
@@ -0,0 +1,38 @@
+/// <reference types="cypress" />
+
+describe("Verify autocomplete dropdown list via webdriveruni", () => {
+  it("Select specific product via autocomplete dropdown list", () => {
+    cy.visit("http://www.webdriveruniversity.com");
+    cy.get("#autocomplete-textfield")
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+
+    cy.get("#myInput").type("a");
+    cy.get("#myInputautocomplete-list > *")
+      .each(($el, index, $list) => {
+        const prod = $el.text();
+        const productToSelect = "Avacado";
+
+        if (prod === productToSelect) {
+          //$el.click();
+          $el.trigger("click");
+          cy.get("#submit-button").click();
+          cy.url().should("include", productToSelect);
+        }
+      })
+      .then(() => {
+        cy.get("#myInput").type("g");
+        cy.get("#myInputautocomplete-list > *").each(($el, index, $list) => {
+          const prod = $el.text();
+          const productToSelect = "Grapes";
+
+          if (prod === productToSelect) {
+            // $el.click();
+            $el.trigger("click");
+            cy.get("#submit-button").click();
+            cy.url().should("include", productToSelect);
+          }
+        });
+      });
+  });
+});
