f4c27ac4c8a470a491dc316abc8d1fef75b3aedd
Bakhyt1987 <bzh.root87@gmail.com>
2024-01-16 14:24:38 +0600

    overriding config js file

--- cypress.config.js
+++ cypress.config.js
@@ -7,8 +7,11 @@ module.exports = defineConfig({
     },
     video: true,
     // testIsolation: false,
+    excludeSpecPattern: "cypress/e2e/other/*.js",
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
     chromeWebSecurity: false,
     experimentalSessionAndOrigin: true,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 12000,
   },
 });
